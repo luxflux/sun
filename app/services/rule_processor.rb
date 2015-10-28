ac4f126ba8b78dd112d99f7dcc2cee0c3b746d85
Raffael Schmid <raffael@yux.ch>
2015-10-28 23:49:09 +0100

    add some flesh to RuleProcessor

--- app/services/rule_processor.rb
+++ app/services/rule_processor.rb
@@ -1,4 +1,37 @@
 class RuleProcessor
   def self.call(rule)
+    new(rule).call
+  end
+
+  attr_reader :rule
+
+  def initialize(rule)
+    @rule = rule
+  end
+
+  def call
+    publisher.publish(message, to_queue: queue)
+    true
+  end
+
+  def publisher
+    @publisher ||= Sneakers::Publisher.new exchange: Global.amqp.exchange,
+                                           exchange_type: Global.amqp.exchange_type
+  end
+
+  def message
+    JSON.dump(location: location_name, port: port_number, state: rule.state)
+  end
+
+  def queue
+    "#{location_name}.port-#{port_number}"
+  end
+
+  def location_name
+    rule.output.location.name
+  end
+
+  def port_number
+    rule.output.number
   end
 end
--- spec/services/rule_processor_spec.rb
+++ spec/services/rule_processor_spec.rb
@@ -2,10 +2,42 @@ require 'rails_helper'
 
 RSpec.describe RuleProcessor do
   let(:rule) { FactoryGirl.create :rule }
+  let(:sneakers_publisher_double) { instance_double(Sneakers::Publisher) }
+  let(:instance) { RuleProcessor.new(rule) }
+
+  before do
+    allow(Sneakers::Publisher).to receive(:new).and_return(sneakers_publisher_double)
+    allow(sneakers_publisher_double).to receive(:publish)
+  end
 
   describe '.call' do
-    subject { RuleProcessor.call(rule) }
+    it 'calls #call on the instance' do
+      rule_processor_double = instance_double(RuleProcessor)
+      expect(RuleProcessor).to receive(:new).with(rule).and_return(rule_processor_double)
+      expect(rule_processor_double).to receive(:call)
+      RuleProcessor.call(rule)
+    end
+  end
 
-    it { is_expected.to eq(nil) }
+  describe '#call' do
+    subject { instance.call }
+
+    it { is_expected.to eq(true) }
+
+    it 'sends all the relevant info in the message' do
+      expected_data = {
+        location: rule.output.location.name,
+        port: rule.output.number,
+        state: rule.state,
+      }.to_json
+      expect(sneakers_publisher_double).to receive(:publish).with(expected_data, to_queue: String)
+      subject
+    end
+  end
+
+  describe '#queue' do
+    subject { instance.queue }
+
+    it { is_expected.to eq("#{rule.output.location.name}.port-#{rule.output.number}") }
   end
 end
