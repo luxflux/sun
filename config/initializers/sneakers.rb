8d1ae7dc047ebbf794c665255fbf2b630d4846a4
Raffael Schmid <raffael@yux.ch>
2016-04-10 21:13:53 +0200

    only one worker with one thread

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,3 +1,3 @@
 require 'sneakers'
-Sneakers.configure workers: 10, threads: 10, log: Rails.root.join('log/sneakers.log')
+Sneakers.configure workers: 1, threads: 1, log: Rails.root.join('log/sneakers.log')
 Sneakers.logger.level = Logger::INFO
